e5dbbcd47a12f6d514f7a5564d5ff9f3841e7d64
Andrea Zavatta <77069789+AndreaZavatta@users.noreply.github.com>
2023-02-17 16:26:24 +0100

    Time: 57 ms (91.72%), Space: 14.7 MB (41.72%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -6,34 +6,35 @@ class Solution(object):
         :rtype: List[int]
         """
         return [self.findFirstOccurrence(nums, target), self.findLastOccurrence(nums, target)]
-        
+    
     def findFirstOccurrence(self, nums, target):
-        hi = len(nums) - 1
         lo = 0
+        hi = len(nums)-1
         result = -1
         while(lo <= hi):
-            
-            mid = (hi + lo) // 2
+            mid = (lo + hi) // 2
             if nums[mid] == target:
                 result = mid
                 hi = mid -1
-            elif nums[mid] > target:
-                hi = mid - 1
-            else:
+            elif nums[mid] < target:
                 lo = mid + 1
+            else:
+                hi = mid -1
         return result
     
     def findLastOccurrence(self, nums, target):
-        hi = len(nums) - 1
         lo = 0
+        hi = len(nums)-1
         result = -1
         while(lo <= hi):
-            mid = (hi + lo) // 2
+            mid = (lo + hi) // 2
             if nums[mid] == target:
                 result = mid
                 lo = mid + 1
-            elif nums[mid] > target:
-                hi = mid - 1
-            else:
+            elif nums[mid] < target:
                 lo = mid + 1
-        return result  
+            else:
+                hi = mid -1
+        return result
+    
+  
